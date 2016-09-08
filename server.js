8863fb8341aa4ebede704f2486e5beb206f94b52
Paul Brown <paul@exe-it.net>
2016-09-08 08:20:37 +0100

    Added delete

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.get('/todos/:id', function (req, res) {
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
-		res.status(404).send();
+		//res.status(404).send();
+		res.status(404).json({"error":"no todo found with that id"});
 	}	
 });
 
@@ -47,6 +48,22 @@ app.post('/todos', function (req, res) {
 	todos.push(body);
 
 	res.json(body);
+});
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (matchedTodo) {
+
+		todos = _.without(todos, matchedTodo);
+
+		res.json(matchedTodo);
+	} else {
+		res.status(404).json({"error":"no todo found with that id"});
+	}	
 });
 
 app.listen(PORT, function() {
