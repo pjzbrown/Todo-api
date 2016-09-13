48e29aab51a5fb8245ec407c72fcb41e3f3ebc71
Paul Brown <paul@exe-it.net>
2016-09-13 17:39:41 +0100

    Adding user/todo association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,51 +19,40 @@ var Todo = sequelize.define('todo', {
 	}
 })
 
+var User = sequelize.define('user', {
+	email:Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
 	//force:true
 }).then(function () {
 	console.log('Everything is synced');
 
-//findOne([options]) -> Promise.<Instance>
-
-	Todo.findOne({
-		where:{
-			id:3
-		}
-	}).then(function(todo) {
-		if (todo){
-			console.log(todo.toJSON());
-		} else {
-			console.log('Todo nor found');
-		}
+	User.findById(1).then(function (user) {
+		user.getTodos({
+			where: {
+				completed:false
+			}
+		}).then(function (todos) {
+			todos.forEach(function (todo) {
+				console.log(todo.toJSON());
+			})
+		})
 	})
 
-	// Todo.create({
-	// 	description: 'Buy the Beast some flowers',
-	// 	//completed:true
-	// }).then(function (todo) {
+	// User.create({
+	// 	email:'paul@example.com'
+	// }).then(function () {
 	// 	return Todo.create({
-	// 		description: 'Go to aqua'
+	// 		description: 'clean yard'
+	// 	}).then(function (todo) {
+	// 		return User.findById(1).then(function (user) {
+	// 			user.addTodo(todo);
+	// 		});
 	// 	});
-	// }).then(function() {
-	// 	//return Todo.findById(1)
-	// 	return Todo.findAll({
-	// 		where: {
-	// 			description:{
-	// 				$like: '%flow%'
-	// 			}
-	// 		}
-	// 	});
-	// }).then(function(todos) {
-	// 	if (todos) {
-	// 		todos.forEach(function(todo) {
-	// 			console.log(todo.toJSON());
-	// 		})
-	// 	} else {
-	// 		console.log('No todo found');
-	// 	}
-	// }).catch(function(e) {
-	// 	console.log(e);
 	// });
 });
 
--- server.js
+++ server.js
@@ -64,7 +64,11 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	body = _.pick(body, 'description', 'completed');
 
 	db.todo.create(body).then(function (todo) {
-		res.json(todo.toJSON());
+		req.user.addTodo(todo).then(function () {
+			return todo.reload();
+		}).then(function(user) {
+			res.json(todo.toJSON());
+		});		
 	}).catch(function(e) {
 		res.status(400).json(e);
 	});
@@ -153,7 +157,7 @@ app.post('/users/login', function(req, res) {
 });
 
 // sync database and launch app
-db.sequelize.sync().then(function () {
+db.sequelize.sync({force:true}).then(function () {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	})
